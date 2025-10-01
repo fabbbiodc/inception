7705e6f1fab8d0078bea14a3146ac01712efd860
fabbbiodc <fabiodicecca@gmail.com>
2025-10-01 15:06:48 +0200

    fixing mariadb

--- srcs/containers/mariadb/init-mariadb.sh
+++ srcs/containers/mariadb/init-mariadb.sh
@@ -12,15 +12,16 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     done
 
     mariadb -u root << EOF
-FLUSH PRIVILEGES;
+-- Drop the anonymous user and test database for security
 DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-CREATE DATABASE ${MYSQL_DATABASE};
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+-- Create the user and database if they don't exist
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+-- Set the root password
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+-- Reload the privileges to apply all changes immediately
 FLUSH PRIVILEGES;
 EOF
 
@@ -29,4 +30,4 @@ else
     echo "Database already initialized."
 fi
 
-exec /usr/bin/mysqld --user=mysql --bind-address=0.0.0.0 --port=3306
+exec /usr/bin/mysqld --user=mysql
