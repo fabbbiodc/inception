96967ea4d0ef50d8de1bbde017c7eff8c09574c7
fabbbiodc <fabiodicecca@gmail.com>
2025-10-01 14:34:02 +0200

    changing mariadb

--- srcs/containers/mariadb/init-mariadb.sh
+++ srcs/containers/mariadb/init-mariadb.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 
-/usr/bin/mysqld --user=mysql --bootstrap << EOF
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Database not found. Initializing..."
+    
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
+    /usr/bin/mysqld --user=mysql &
+    
+    while ! mysqladmin ping --silent; do
+        sleep 1
+    done
+
+    mariadb -u root << EOF
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-
 CREATE DATABASE ${MYSQL_DATABASE};
 CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
-
 EOF
 
-exec /usr/bin/mysqld --user=mysql
+    mysqladmin shutdown
+else
+    echo "Database already initialized."
+fi
 
+exec /usr/bin/mysqld --user=mysql
