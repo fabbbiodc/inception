fdfee058c7904f2bd6a38c152cdfcea4e164aa72
fabbbiodc <fabiodicecca@gmail.com>
2025-10-06 14:43:25 +0200

    wordpress setup

---
+++ srcs/requirements/wordpress/tools/wp-init.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -e
+
+echo "Waiting for MariaDB to be ready..."
+while ! mysqladmin ping -h"mariadb" --silent; do
+    sleep 1
+done
+echo "MariaDB is ready."
+
+WEB_ROOT=/var/www/html
+
+if [ -f "$WEB_ROOT/wp-config.php" ]; then
+    echo "WordPress is already installed."
+else
+    echo "WordPress not found. Starting installation..."
+
+    mkdir -p $WEB_ROOT
+        cd $WEB_ROOT
+
+    wp core download --allow-root
+
+    wp config create \
+        --dbname=$DB_NAME \
+        --dbuser=$DB_USER \
+        --dbpass=$DB_PASSWORD \
+        --dbhost=mariadb \
+        --allow-root
+
+    wp core install \
+        --url=$DOMAIN_NAME \
+        --title="Inception" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --allow-root
+
+    wp user create \
+        $WP_USER \
+        $WP_USER_EMAIL \
+        --role=author \
+        --user_pass=$WP_USER_PASSWORD \
+        --allow-root
+
+    echo "WordPress installation complete."
+
+fi
+
+echo "Starting PHP-FPM..."
+
+exec "$@"
