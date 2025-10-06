061ab32203861ea90b6380cae90dc246afa7cce5
fabbbiodc <fabiodicecca@gmail.com>
2025-10-06 16:20:58 +0200

    mariadb port conf

--- srcs/requirements/mariadb/tools/maria-init.sh
+++ srcs/requirements/mariadb/tools/maria-init.sh
@@ -11,7 +11,7 @@ else
 
 	mariadb-install-db --user=mysql --datadir="$DATADIR"
 
-	mariadbd --user=mysql --datadir="$DATADIR" & pid="$!"
+	mariadbd --user=mysql --datadir="$DATADIR" --bind-address=0.0.0.0 --port=3306 & pid="$!"
 
 	for i in {30..0}; do
 		if mariadb-admin ping -h"localhost" --silent; then
