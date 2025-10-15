b6f75d203641efdd5b8b2c9b4f344f96b05a4130
fabbbiodc <fabiodicecca@gmail.com>
2025-10-15 11:35:46 +0200

    wip adminer

---
+++ srcs/requirements/bonus/adminer/tools/adminer-init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+ADMINER_DIR="/var/www/html/adminer"
+ADMINER_FILE="$ADMINER_DIR/index.php"
+
+if [ ! -d "$ADMINER_DIR" ]; then
+    echo "Creating Adminer directory..."
+    mkdir -p "$ADMINER_DIR"
+fi
+
+if [ ! -f "$ADMINER_FILE" ]; then
+    echo "Downloading Adminer..."
+    curl -L "[https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php](https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php)" -o "$ADMINER_FILE"
+    chown -R www-data:www-data "$ADMINER_DIR"
+    echo "Adminer downloaded successfully."
+else
+    echo "Adminer already exists."
+fi
+
+echo "Starting PHP-FPM for Adminer..."
+
+exec "$@"
