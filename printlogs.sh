de13cb8cecda643a8a1be708ac3ba8c5dc3528c4
fabbbiodc <fabiodicecca@gmail.com>
2025-10-21 18:58:51 +0200

    static site containerized

--- printlogs.sh
+++ printlogs.sh
@@ -2,7 +2,7 @@
 
 read -p "Lines to print from each container: " num_lines
 
-for container in mariadb wordpress nginx redis adminer ftp; do
+for container in mariadb wordpress nginx redis adminer ftp portainer static_site; do
     echo "=== $container logs ==="
     docker logs "$container" -n "$num_lines"
     echo
